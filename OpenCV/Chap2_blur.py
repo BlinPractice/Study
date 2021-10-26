76381ebbf65b057d4448eb213341a6b8ecbb4a07
Goblin <86243364+PyBlin@users.noreply.github.com>
2021-10-27 02:23:25 +0900

    Create Chap2_blur.py

---
+++ OpenCV/Chap2_blur.py
@@ -0,0 +1,10 @@
+import cv2
+
+img = cv2.imread("Resources/Lenna.png")
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
+
+cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
+cv2.waitKey(0)
